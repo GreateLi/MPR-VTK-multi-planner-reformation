5d6a3d1bbb3ae254825b8cca9bf7a9fe5d531a8e
GreateLi <36566865+GreateLi@users.noreply.github.com>
2023-05-25 15:28:45 +0800

    Add files via upload
    C++

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include "PointCloudVisualizer.h"
+//#include <QtWidgets/QApplication>
+#include <QApplication>
+#include<QDebug>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    PointCloudVisualizer w;
+    w.show();
+    qDebug()<<"========";
+    return a.exec();
+}
